69cbc039cf6ad7edd7a20a946cc3bbb06672348c
IanMIsik <ianmisik9@gmail.com>
2023-12-11 15:40:22 +0000

    Register page first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './index.css';
+import Register from './Register';
+
+function App() {
+  return (
+    <main className='App'>
+
+      <Register/>
+
+    </main>
+  );
+}
+
+export default App;
